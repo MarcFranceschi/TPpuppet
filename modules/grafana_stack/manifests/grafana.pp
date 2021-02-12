799101c5f50f54ff7fb4c3a43a572c67263aefa9
MarcFranceschi <marc.franceschi@epitech.eu>
2021-02-12 17:06:05 +0100

    modifs

--- modules/grafana_stack/manifests/grafana.pp
+++ modules/grafana_stack/manifests/grafana.pp
@@ -3,16 +3,16 @@
 # A description of what this class does
 #
 # @example
-#   include grafana::grafana
+#   include grafana_stack::grafana
 class grafana_stack::grafana {
-  class { 'grafana':
+    class { 'grafana':
       cfg => {
         app_mode => 'production',
         server   => {
           http_port     => 8080,
         },
         database => {
-          type     => 'mysql',
+          type     => 'influxdb',
           host     => '127.0.0.1:3306',
           name     => 'grafana',
           user     => 'root',
--- modules/grafana_stack/spec/classes/grafana_spec.rb
+++ modules/grafana_stack/spec/classes/grafana_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'lamp::grafana' do
+describe 'grafana_stack::grafana' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
