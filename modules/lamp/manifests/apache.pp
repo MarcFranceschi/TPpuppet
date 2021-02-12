55b2b060383a62e9eef3708c98e66503dbff9d75
MarcFranceschi <marc.franceschi@epitech.eu>
2021-02-12 16:21:41 +0100

    update manifests grafana

--- modules/lamp/manifests/apache.pp
+++ modules/lamp/manifests/apache.pp
@@ -7,9 +7,9 @@
 class lamp::apache {
   class { '::apache' :
       default_vhost => false,
- }
- apache::vhost { 'vhost.example.com':
+  }
+  apache::vhost { 'vhost.example.com':
   port    => '80',
   docroot => '/var/www/vhost',
-}
+  }
 }
