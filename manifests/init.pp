438886341152cccf3f579eda9e1bdbe8ac5f154c
Marc Franceschi <marc.franceschi@epitech.eu>
2021-04-30 16:07:28 +0200

    regles fw

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,15 @@ node default {
   include grafana_stack::grafana
   include influxdb
   include grafana_stack::telegraf
+
+class my_fw::pre {
+  Firewall {
+    require => undef,
+  }
+  # Default firewall rules
+    firewall { 'allow http https and ssh access':
+    dport  => [80, 443, 22],
+    proto  => 'tcp',
+    action => 'accept',
+  }
 }
