c6c4e5ff3111fff6dbcc4272de495a7383fc3424
Marc Franceschi <marc.franceschi@epitech.eu>
2021-04-30 16:13:09 +0200

    corr init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,14 +6,15 @@ node default {
   include influxdb
   include grafana_stack::telegraf
 
-class my_fw::pre {
-  Firewall {
-    require => undef,
-  }
-  # Default firewall rules
-    firewall { 'allow http https and ssh access':
-    dport  => [80, 443, 22],
-    proto  => 'tcp',
-    action => 'accept',
+  class my_fw::pre {
+    Firewall {
+      require => undef,
+    }
+    # Default firewall rules
+      firewall { 'allow http https and ssh access':
+      dport  => [80, 443, 22],
+      proto  => 'tcp',
+      action => 'accept',
+    }
   }
 }
